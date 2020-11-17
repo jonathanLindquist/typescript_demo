3997183f73c0a74a47bdb88d24436ec7a51b7db2
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2020-11-17 15:43:41 -0600

    initial test

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log("Your code goes here!!!");
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    // console.log(typeof n1);
+    let result = n1 + n2;
+	if (showResult) {
+		console.log(phrase + result);
+	}
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = typeof number1 === 'number' && typeof number2 === 'number';
+const phrase = 'Result is ';
+
+const result = add(number1, number2, printResult, phrase);
+console.log(result);
