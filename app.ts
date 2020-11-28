003a79441cc70383937935f1276bc88689e3f6b2
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2020-11-28 15:50:25 -0600

    type aliases

--- app.ts
+++ app.ts
@@ -1,19 +1,22 @@
-function combine(input1: number | string, input2: number | string, resultType: 'asText' | 'asNumber') {
+type Combinable = number | string;
+type ConversionDescriptor = 'asText' | 'asNumber';
+
+function combine(input1: Combinable, input2: Combinable, resultType: ConversionDescriptor) {
 	let result;
 
-	if (typeof input1 === 'number' && typeof input2 === 'number' || resultType === 'asNumber') {
+	if ((typeof input1 === 'number' && typeof input2 === 'number') || resultType === 'asNumber') {
 		result = +input1 + +input2;
 	} else {
 		result = input1.toString() + input2.toString();
-    }
+	}
 
-    return result;
-    
-    // if (resultType === 'asNumber') {
-    //     return +result;
-    // } else {
-    //     return result.toString();
-    // }
+	return result;
+
+	// if (resultType === 'asNumber') {
+	//     return +result;
+	// } else {
+	//     return result.toString();
+	// }
 }
 
 const combinedAges = combine(30, 26, 'asNumber');
