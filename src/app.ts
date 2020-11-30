d5031ab0c51de7f76cc109819ce166c8ec4d681c
Jonathan Lindquist <jonathan.lindquist17@gmail.com>
2020-11-30 13:52:26 -0600

    next-gen JS redo

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,46 @@
 // code goes here
+
+const fun = (item: string): void => {
+	console.log(item);
+};
+
+fun('hello jonathan');
+
+const hobbies = [ 'sports', 'cookings' ];
+console.log(hobbies[0]);
+
+const activeHobbies = [ 'water boarding','hiking', ...hobbies ];
+console.log(activeHobbies);
+
+const person = {
+	firstName: 'Jonathan',
+	age: 31
+};
+
+const copiedPerson = {
+	...person
+};
+copiedPerson.firstName = 'copied Jonathan';
+
+console.log(person);
+console.log(copiedPerson);
+
+const restFunction = (...args: number[]) => {
+	const result = args.reduce((total, currentValue) => {
+		return total + currentValue;
+	}, 0);
+	console.log(result);
+};
+
+restFunction(1, 2, 3, 4, 5);
+
+const [hobby1, hobby2, ...remainingHobbies] = activeHobbies;
+console.log(hobby1)
+console.log(hobby2)
+console.log(remainingHobbies)
+
+
+const { firstName: personName, age } = person;
+
+console.log(personName)
+console.log(age)
